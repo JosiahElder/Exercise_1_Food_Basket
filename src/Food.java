2822a01a056ea862cc2b819e0200d04830844df9
JosiahElder <@JosiahElder>
2023-03-31 20:57:27 -0700

    Next Commit

--- src/Food.java
+++ src/Food.java
@@ -1,25 +1,31 @@
-public class Food {
+public class Food
+{
     private String name;
     private String type;
 
-    public Food(String name, String type) {
+    public Food(String name, String type)
+    {
         this.name = name;
         this.type = type;
     }
 
-    public String eatIt() {
+    public String eatIt()
+    {
         return "You just ate the " + name + ".";
     }
 
-    public String denyIt() {
+    public String denyIt()
+    {
         return "Not eating that! I hate " + type + "!";
     }
 
-    public String getName() {
+    public String getName()
+    {
         return name;
     }
 
-    public String getType() {
+    public String getType()
+    {
         return type;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
-public class Main {
-    public static void main(String[] args) {
-        Food[] foodF = new Food[3];
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Food[] picnicBasket = new Food[4];
+        picnicBasket[0] = new Food("Banana", "fruit");
+        picnicBasket[1] = new Food("Steak", "meat");
+        picnicBasket[2] = new Food("Spinach", "vegetable");
+        picnicBasket[3] = new Food("Apple", "fruit");
 
-
+        for (int i = 0; i < picnicBasket.length; i++)
+        {
+            Food food = picnicBasket[i];
+            if (food.getType().equals("vegetable"))
+            {
+                System.out.println(food.denyIt());
+            }
+            else
+            {
+                System.out.println(food.eatIt());
+            }
+        }
     }
 }
